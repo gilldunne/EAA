8744f838147003d9d0d4d22ea506e04be72703e4
gilldunne <x00094469@ittd.ie>
2014-09-26 15:43:41 +0100

    Script file converted to executible bash script

--- cpucount.sh
+++ cpucount.sh
@@ -1,1 +1,11 @@
-cat /proc/cpuinfo | grep processor | wc -l
+#!/bin/bash
+
+#: Title : cpucount.sh
+#: Date : 26/09/2014
+#: Author : Gill Dunne
+#: Version : V1.0
+#: Description : Counting CPU on guest
+#: Options : 
+
+grep processor /proc/cpuinfo | wc -l
+
